4282e83fb4968efcb0ad6315fef65cbfeef4b541
wanghuanle <wanghuanle@qq.com>
2020-11-14 12:52:35 +0800

    配置logback日志与多环境profile

--- soft-news/soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news/soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.soft1851.user.controller;
 
 import com.soft185.api.controller.user.HelloControllerApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2020/11/13
  **/
 @RestController
+@Slf4j
 public class HelloController implements HelloControllerApi {
 
 
     @Override
     public Object hello() {
+        log.info("info:hello");
+        log.info("warn:hello");
+        log.info("error:hello");
         return "hello";
+
     }
 }
